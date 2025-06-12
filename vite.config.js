61ecf025eb01984fe84a586a862911dcbf43730f
logger-09dapple <138261362+logger-09dapple@users.noreply.github.com>
2025-06-11 23:54:59 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,1 +1,10 @@
-import { defineConfig } from 'vite'\nimport react from '@vitejs/plugin-react'\n\n// https://vitejs.dev/config/\nexport default defineConfig({\n  plugins: [react()],\n  base: '/badminton-league-manager/',\n  build: {\n    outDir: 'dist',\n  },\n})
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: '/badminton-league-app/',
+  build: {
+    outDir: 'dist',
+  },
+})
